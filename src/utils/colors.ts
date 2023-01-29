ad20a079ee68c9696f565958131e41d7cf6edb59
Eylon <eylon.perets@gmail.com>
2023-01-29 17:40:53 +0200

    setup

---
+++ src/utils/colors.ts
@@ -0,0 +1,10 @@
+const colors = {
+  emptyBg: '#121213',
+  emptyBorder: '#3a3a3c',
+  noLetterBg: '#3a3a3a',
+  wrongOrderBg: '#b59f3b',
+  rightPlaceBg: '#538d4e',
+  letter: '#ffffff',
+};
+
+export default colors;
